b142c034fcc9294f8ade6439ec646a5b10694dba
3m1l10j4v13r4qu1n0 <aquinoemiliojavier@gmail.com>
2025-07-28 19:49:12 -0300

    se reordena los arhivos y crean oros ejemplo layout.html

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, flash, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 
-from ..models import User
+from app.models.modelo_login import User
 from .. import db, login_manager
 from ..forms import LoginForm, RegisterForm
 
